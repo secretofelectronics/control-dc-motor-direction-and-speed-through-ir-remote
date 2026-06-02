312d722b234709ea991a2fa54133ca4d0dcffdc0
secretofelectronics <secretofelectronics@gmail.com>
2026-06-02 16:56:47 +0530

    Update ir_remote_dc_motor_speed_controller.ino

--- ir_remote_dc_motor_speed_controller.ino
+++ ir_remote_dc_motor_speed_controller.ino
@@ -6,14 +6,15 @@
 #define IN1 7
 #define IN2 8
 
-#define IR_UP      0xF10EFF00
-#define IR_DOWN    0xE51AFF00
+#define IR_UP      0xE619FF00
+#define IR_DOWN    0xF906FF00
 #define IR_POWER   0xF609FF00
-#define IR_REVERSE 0xE11EFF00
+#define IR_REVERSE 0xEA15FF00
 
-int speedLevel = 0;
 bool motorState = false;
-bool direction = true;
+bool direction = true;   // true=Forward, false=Reverse
+
+int speedLevel = 1;      // 1=Slow, 2=Medium, 3=Fast
 
 void setup()
 {
@@ -22,8 +23,6 @@ void setup()
   pinMode(IN2, OUTPUT);
 
   IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
-
-  Serial.begin(9600);
 
   updateMotor();
 }
@@ -34,22 +33,19 @@ void loop()
   {
     uint32_t code = IrReceiver.decodedIRData.decodedRawData;
 
-    Serial.print("Code: ");
-    Serial.println(code, HEX);
-
-    switch(code)
+    switch (code)
     {
       case IR_POWER:
         motorState = !motorState;
         break;
 
       case IR_UP:
-        if(speedLevel < 4)
+        if (speedLevel < 3)
           speedLevel++;
         break;
 
       case IR_DOWN:
-        if(speedLevel > 0)
+        if (speedLevel > 1)
           speedLevel--;
         break;
 
@@ -66,7 +62,7 @@ void loop()
 
 void updateMotor()
 {
-  if(direction)
+  if (direction)
   {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
@@ -79,23 +75,23 @@ void updateMotor()
 
   int pwmValue = 0;
 
-  switch(speedLevel)
+  switch (speedLevel)
   {
-    case 0: pwmValue = 0; break;
-    case 1: pwmValue = 64; break;
-    case 2: pwmValue = 128; break;
-    case 3: pwmValue = 192; break;
-    case 4: pwmValue = 255; break;
+    case 1:
+      pwmValue = 85;   // Slow
+      break;
+
+    case 2:
+      pwmValue = 170;  // Medium
+      break;
+
+    case 3:
+      pwmValue = 255;  // Fast
+      break;
   }
 
-  if(!motorState)
+  if (!motorState)
     pwmValue = 0;
 
   analogWrite(ENA, pwmValue);
-
-  Serial.print("Speed Level: ");
-  Serial.print(speedLevel);
-
-  Serial.print(" PWM: ");
-  Serial.println(pwmValue);
 }
